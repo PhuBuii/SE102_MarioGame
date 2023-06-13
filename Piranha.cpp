94b255e0e849ffc711532437efd06e45946395f2
PhuBuii <20520273@gm.uit.edu.vn>
2023-06-13 12:02:11 +0700

    fix piranha wait in 3s

--- Piranha.cpp
+++ Piranha.cpp
@@ -13,7 +13,7 @@ void CPiranha::Render()
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_PIRANHA)->Render(x, y);
 
-	//RenderBoundingBox();
+	RenderBoundingBox();
 }
 
 void CPiranha::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
@@ -21,11 +21,15 @@ void CPiranha::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	y += vy * dt;
 
 
-	if ((state == PIRANHA_STATE_UP) && (GetTickCount64() - up_start > PIRANHA_UP_TIMEOUT))
+	if ((state == PIRANHA_STATE_UP) && (GetTickCount64() - up_start > PIRANHA_TIMEOUT))
 	{
 		SetState(PIRANHA_STATE_DOWN);
 	}
-	else if ((state == PIRANHA_STATE_DOWN) && (GetTickCount64() - up_start > PIRANHA_UP_TIMEOUT))
+	else if ((state == PIRANHA_STATE_DOWN) && (GetTickCount64() - up_start > PIRANHA_TIMEOUT))
+	{
+		SetState(PIRANHA_STATE_WAIT);
+	}
+	else if ((state == PIRANHA_STATE_WAIT) && (GetTickCount64() - wait_start > PIRANHA_WAIT_TIMEOUT))
 	{
 		SetState(PIRANHA_STATE_UP);
 	}
@@ -41,11 +45,18 @@ void CPiranha::SetState(int state)
 	{
 	case  PIRANHA_STATE_UP:
 		vy = -PIRANHA_SPEED;
+		wait_start = -1;
 		up_start = GetTickCount64();
 		break;
 	case  PIRANHA_STATE_DOWN:
 		vy = PIRANHA_SPEED;
+		wait_start = -1;
 		up_start = GetTickCount64();
+		break;
+	case  PIRANHA_STATE_WAIT:
+		vy = 0;
+		wait_start = GetTickCount64();
+		up_start = -1;
 		break;
 	}
 }
--- Piranha.h
+++ Piranha.h
@@ -9,14 +9,16 @@
 
 #define PIRANHA_WIDTH 16
 #define PIRANHA_BBOX_WIDTH 16
-#define PIRANHA_BBOX_HEIGHT 24
+#define PIRANHA_BBOX_HEIGHT 22
 
 #define PIRANHA_STATE_UP 100
 #define PIRANHA_STATE_DOWN 101
+#define PIRANHA_STATE_WAIT 102
 
 #define PIRANHA_SPEED 0.025f
 
-#define PIRANHA_UP_TIMEOUT 1000
+#define PIRANHA_TIMEOUT 1100
+#define PIRANHA_WAIT_TIMEOUT 3000
 
 
 
@@ -25,15 +27,21 @@ class CPiranha : public CGameObject
 protected:
 	virtual void OnNoCollision(DWORD dt) { return; };
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e) { return; };
-	ULONGLONG up_start;
+	ULONGLONG up_start, wait_start;
 public:
 	CPiranha(float x, float y) : CGameObject(x, y) {
 		up_start = -1;
-		SetState(PIRANHA_STATE_UP);
+		wait_start = -1;
+		SetState(PIRANHA_STATE_WAIT);
 
 	};
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	void Render();
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void SetState(int state);
+	int IsBlocking() { return 0; }
+	int IsCollidable()
+	{
+		return (state != PIRANHA_STATE_WAIT);
+	}
 };
